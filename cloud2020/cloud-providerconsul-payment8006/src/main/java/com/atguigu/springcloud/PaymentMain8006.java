6d7bb497054b7dcad58f1e0059558e9371c5b810
QiHang <553446603@qq.com>
2021-03-16 17:29:08 +0800

    consul

---
+++ cloud2020/cloud-providerconsul-payment8006/src/main/java/com/atguigu/springcloud/PaymentMain8006.java
@@ -0,0 +1,14 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author lqh
+ * @create 2021/3/16 下午4:02
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8006 {
+
+}
