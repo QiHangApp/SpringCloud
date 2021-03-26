c9ac857fed3c2fb9dea7e0e85b4b27dbd61de6f1
QiHang <553446603@qq.com>
2021-03-26 17:02:26 +0800

    openfeign

---
+++ cloud2020/cloud-consumer-feign-order80/src/main/java/com/atguigu/springcud/config/FignConfig.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcud.config;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author lqh
+ * @create 2021/3/26 下午3:00
+ */
+
+@Configuration
+public class FignConfig {
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+}
--- cloud2020/cloud-consumer-feign-order80/src/main/java/com/atguigu/springcud/controller/OrderFeignController.java
+++ cloud2020/cloud-consumer-feign-order80/src/main/java/com/atguigu/springcud/controller/OrderFeignController.java
@@ -6,7 +6,6 @@ import com.atguigu.springcud.service.PaymentFeignService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -26,5 +25,11 @@ public class OrderFeignController {
     @GetMapping(value = "/consumer/payment/get/{id}")
     public CommonResult<Payment> getPaymentbyId(@PathVariable("id") Long id) {
         return paymentFeignService.getPaymentById(id);
+    }
+
+    @GetMapping(value = "/consumer/payment/feign/timeout")
+    public String paymentFeignTimeout() {
+        //openfeitn-ribbon, 客户端一般默认等待1秒钟
+        return paymentFeignService.paymentFeignTimeout();
     }
 }
