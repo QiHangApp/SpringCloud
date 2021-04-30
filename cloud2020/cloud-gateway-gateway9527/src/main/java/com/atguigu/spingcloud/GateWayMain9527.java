1459f57e8fcfe8667e8c2698097f3e65a958e57e
QiHang <553446603@qq.com>
2021-04-30 17:23:54 +0800

    GateWayConfig

---
+++ cloud2020/cloud-gateway-gateway9527/src/main/java/com/atguigu/spingcloud/GateWayMain9527.java
@@ -0,0 +1,18 @@
+package com.atguigu.spingcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author lqh
+ * @create 2021/4/30 下午2:47
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
